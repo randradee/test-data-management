9e0da88bb7bdcfeaa9e5e1f6806d2452a84b568a
Renato Andrade <dev.renatopandrade@gmail.com>
2023-07-17 18:16:02 -0300

    configs iniciais

---
+++ src/main/java/com/randradee/test/data/management/TestDataManagementApplication.java
@@ -0,0 +1,13 @@
+package com.randradee.test.data.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDataManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDataManagementApplication.class, args);
+	}
+
+}
